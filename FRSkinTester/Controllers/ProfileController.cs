5f510e3b10466bcd2135bba8226ca04b726103ea
Perry van Velzen <perryvvelzen@gmail.com>
2019-03-13 23:25:19 +0100

    asd

---
+++ FRSkinTester/Controllers/ProfileController.cs
@@ -0,0 +1,36 @@
+﻿using FRSkinTester.Infrastructure;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FRSkinTester.Controllers
+{
+    public class ProfileController : BaseController
+    {
+        [Authorize]
+        [Route("Profile")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Profile/{username}")]
+        public ActionResult Index(string username)
+        {
+            using (var ctx = new DataContext())
+            {
+                var user = ctx.Users.FirstOrDefault(x => x.UserName.ToLower() == username.ToLower());
+                if(user == null)
+                {
+                    TempData["Error"] = $"No user with username '{username}' could be found";
+                    return RedirectToRoute("Home");
+                }
+                else if (user.Privacy == Infrastructure.DataModels.Privacy.HideAll)
+                {
+                    TempData["Error"] = "This user's profile is set to private";
+                    return RedirectToRoute("Home");
+                }
+                return View();
+            }
+        }
+    }
+}
--- FRSkinTester/Infrastructure/DataModels/AccountModels/User.cs
+++ FRSkinTester/Infrastructure/DataModels/AccountModels/User.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 
 namespace FRSkinTester.Infrastructure.DataModels
 {
+    [Flags]
+    public enum Privacy
+    {
+        HidePreviews = 1,
+        HideSkins = 2,
+        HideAll = 3
+    }
+
     public class User : IdentityUser<int, UserLogin, UserRole, UserClaim>
     {
         public int? FRId { get; set; }
         public string FRName { get; set; }
+
+        public Privacy Privacy { get; set; }
 
         public virtual ICollection<Skin> Skins { get; set; } = new HashSet<Skin>();
     }
