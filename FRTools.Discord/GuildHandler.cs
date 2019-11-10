598c72f97fd6a1e3250d21a198003e06d66f7815
Perry van Velzen <perryvvelzen@gmail.com>
2019-11-10 11:02:08 +0100

    Started discord bot dev

---
+++ FRTools.Discord/GuildHandler.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace FRTools.Discord
+{
+    public class GuildHandler
+    {
+        private readonly SocketGuild _guild;
+        private readonly IUnityContainer _container;
+        private readonly IDiscordClient _client;
+        private readonly CommandService _globalCommandService;
+
+        public GuildHandler(SocketGuild guild, IUnityContainer container, IDiscordClient client, CommandService globalCommandService)
+        {
+            _guild = guild;
+            _container = container;
+            _client = client;
+            _globalCommandService = globalCommandService;
+        }
+    }
+}
---
+++ FRTools.Discord/Program.cs
@@ -0,0 +1,56 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace FRTools.Discord
+{
+    class Program
+    {
+        private static DiscordSocketClient client;
+        private static IUnityContainer _container = new UnityContainer();
+        private CommandService _commandService;
+        private readonly Dictionary<ulong, GuildHandler> _handlers = new Dictionary<ulong, GuildHandler>();
+
+
+        static void Main()
+        {
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel =
+#if DEBUG
+                LogSeverity.Debug
+#else
+                LogSeverity.Debug
+#endif
+
+            });
+            client.Log += Client_Log;
+            new Program().Start(client).GetAwaiter().GetResult();
+        }
+
+        private static Task Client_Log(LogMessage msg)
+        {
+            return Task.Run(() => Console.WriteLine($"[{msg.Severity}] {msg.Message}"));
+        }
+
+        public async Task Start(DiscordSocketClient client)
+        {
+            _container.RegisterFactory<CommandService>(GetCommandService);
+
+            _commandService = _container.Resolve<CommandService>();
+        }
+
+        private CommandService GetCommandService(IUnityContainer container)
+        {
+            var commandService = new CommandService();
+
+            return commandService;
+        }
+    }
+}
