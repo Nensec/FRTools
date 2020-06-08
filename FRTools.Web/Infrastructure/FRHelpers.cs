ab1f8559c5c20979f314a67689257766c5e2388b
Perry van Velzen <perryvvelzen@gmail.com>
2020-06-08 16:35:52 +0200

    Updated the URL used to navigate to dragon profile pages to the new URL used by Flight Rising

--- FRTools.Web/Infrastructure/FRHelpers.cs
+++ FRTools.Web/Infrastructure/FRHelpers.cs
@@ -21,7 +21,7 @@ namespace FRTools.Web.Infrastructure
         //                                          https://www1.flightrising.com/dgen/dressing-room/dummy?breed=18&gender=0.png
         public const string DressingRoomDummyUrl = "https://www1.flightrising.com/dgen/dressing-room/dummy?breed={0}&gender={1}";
         public const string ScryerUrl = "https://flightrising.com/includes/scryer_getdragon.php?zord={0}";
-        public const string DragonProfileUrl = "https://flightrising.com/main.php?dragon={0}";
+        public const string DragonProfileUrl = "https://www1.flightrising.com/dragon/{0}";
         public const string DressingRoomDragonApparalUrl = "https://www1.flightrising.com/dgen/dressing-room/dragon?did={0}&apparel={1}";
         public const string DressingRoomDummyApparalUrl = "https://www1.flightrising.com/dgen/dressing-room/dummy?breed={0}&gender={1}&apparel={1}";
 
