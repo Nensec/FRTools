addd706bd830a04f32093e4416516eb45673abc9
Perry van Velzen <perryvvelzen@gmail.com>
2020-09-13 21:17:24 +0200

    The colors of the pie charts (skin statistics) now reflect the actual color of the color used

---
+++ FRTools.Data/ColorAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace FRTools.Data
+{
+    public class ColorAttribute : Attribute
+    {
+        public string ColorHexCode { get; }
+
+        public ColorAttribute(string colorHexCode)
+        {
+            ColorHexCode = colorHexCode;
+        }
+    }
+
+    public static class ColorHelper
+    {
+        public static string GetHex(this Color color) => (typeof(Color).GetMember(color.ToString()).FirstOrDefault()?.GetCustomAttributes(typeof(ColorAttribute), false).FirstOrDefault() as ColorAttribute).ColorHexCode;
+    }
+}
