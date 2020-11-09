cfbc867f8d1fa4aab6a164c54ac7d054f6088a66
Perry van Velzen <perryvvelzen@gmail.com>
2020-11-09 21:46:48 +0100

    Added privacy policy and separate contact info page in prep for adsense

--- FRTools.Web/Controllers/HomeController.cs
+++ FRTools.Web/Controllers/HomeController.cs
@@ -9,5 +9,11 @@ namespace FRTools.Web.Controllers
 
         [Route("404", Name = "NotFound")]
         public ActionResult NotFound() => View();
+
+        [Route("privacy", Name = "Privacy")]
+        public ActionResult Privacy() => View();
+
+        [Route("contact", Name = "Contact")]
+        public ActionResult Contact() => View();
     }
 }
