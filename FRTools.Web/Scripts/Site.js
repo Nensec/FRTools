94d5d4a717fb3bbf226f7d96b443ac805aa0df3a
Perry van Velzen <perryvvelzen@gmail.com>
2021-03-20 02:17:25 +0100

    Added bootstrap 4 based layout so it can be chosen per view Also included is the first iteration of a dark mode toggle

---
+++ FRTools.Web/Scripts/Site.js
@@ -0,0 +1,21 @@
+﻿copyOrGenerateUrl = (e, b, t, id) => {
+    if (e.value != '') {
+        e.select();
+        document.execCommand('copy');
+        b.innerHTML = 'Copied!';
+    }
+    else {
+        b.innerHTML = 'Fetching..';
+        $.get({
+            url: '@Url.RouteUrl("GetShareUrl")',
+            data: { type: t, id: id },
+            success: d => { e.value = d; copyText(e, b); }
+        }).fail(() => b.innerHTML = 'Error!');
+    }
+}
+
+copyText = (e, b) => {
+    e.select();
+    document.execCommand('copy');
+    b.innerHTML = 'Copied!';
+}
