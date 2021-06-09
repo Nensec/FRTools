82330ec8b5344470306d312ff4172eadae0547e0
Perry van Velzen <perryvvelzen@gmail.com>
2021-06-09 21:44:04 +0200

    Added some logging to the item fetcher and removed the random interval in between searches now that I am not retrieing thousands of items

--- FRTools.MS.ItemFetcher/Program.cs
+++ FRTools.MS.ItemFetcher/Program.cs
@@ -21,8 +21,14 @@ namespace FRTools.MS.ItemFetcher
             var settings = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
             int maxTries = 3;
 
-            if (DateTime.TryParse(settings.AppSettings.Settings["LastSuccess"]?.Value, out var lastSuccess) && DateTime.UtcNow > lastSuccess.AddDays(1))            
-                maxTries += (int)(DateTime.UtcNow - lastSuccess.AddDays(1)).TotalHours;            
+            var hasLastSuccess = DateTime.TryParse(settings.AppSettings.Settings["LastSuccess"]?.Value, out var lastSuccess);
+            if (hasLastSuccess && DateTime.UtcNow > lastSuccess.AddDays(1))
+            {
+                maxTries += (int)(DateTime.UtcNow - lastSuccess.AddDays(1)).TotalHours;
+                Console.WriteLine($"Last succesful bout of skins were found at {lastSuccess}, which makes the max tries to be {maxTries} attempts");
+            }
+            else
+                Console.WriteLine($"No last succes found in config, max tries is {maxTries} attempts");            
 
             _serviceBus = new QueueClient(settings.AppSettings.Settings["AzureSBConnString"].Value, settings.AppSettings.Settings["AzureSBQueueName"].Value);
             await _serviceBus.SendAsync(new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new GenericMessage(MessageCategory.ItemFetcher, "Started")))));
@@ -45,20 +51,21 @@ namespace FRTools.MS.ItemFetcher
                     }
                     else
                         _noItemFoundCounter++;
-                    await Task.Delay(random.Next(75, 500));
+                    await Task.Delay(100);
                 }
 
                 var count = ctx.ChangeTracker.Entries().Count();
                 Console.WriteLine($"Done for now, saving {count} items.");
                 await ctx.SaveChangesAsync();
 
-                if(count > 0)
+                if (count > 0)
                 {
                     // This gets stored in app.config, which gets wiped every deploy but is good enough to store a temp value in between instances
                     // Since if the value is not present we use the default, which will be the vast majority of times anyway
                     settings.AppSettings.Settings["LastSuccess"].Value = DateTime.UtcNow.ToString();
+                    Console.WriteLine($"Since skins were found, saving last success in config at {settings.AppSettings.Settings["LastSuccess"].Value}");
                     settings.Save();
-                }    
+                }
             }
             await _serviceBus.CloseAsync();
         }
