4ab7e4e90ff1e38cfb077aeb5abb5ddbcf097982
Perry van Velzen <perryvvelzen@gmail.com>
2021-11-19 02:46:39 +0100

    Flash tracker now posts to tumblr when a new flash sale is found, formatting is not exactly final yet but gotta see how it looks over time

--- FRTools.MS.FlashTracker/Program.cs
+++ FRTools.MS.FlashTracker/Program.cs
@@ -1,15 +1,20 @@
-﻿using FRTools.Common;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using DontPanic.TumblrSharp;
+using DontPanic.TumblrSharp.Client;
+using DontPanic.TumblrSharp.OAuth;
+using FRTools.Common;
 using FRTools.Data;
 using FRTools.Data.DataModels.FlightRisingModels;
 using FRTools.Data.Messages;
 using HtmlAgilityPack;
 using Microsoft.Azure.ServiceBus;
 using Newtonsoft.Json;
-using System;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FRTools.MS.FlashTracker
 {
@@ -36,12 +41,120 @@ namespace FRTools.MS.FlashTracker
                 if (item == null)
                 {
                     item = FRHelpers.FetchItem(itemId);
-                    if(item != null)
+                    if (item != null)
                         await _serviceBus.SendAsync(new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new NewItemMessage(MessageCategory.FlashTracker, item)))));
                 }
                 if (item.FlashSales.All(x => x.RemovedAt != null))
                 {
                     await _serviceBus.SendAsync(new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new FlashSaleMessage(MessageCategory.FlashTracker, item), new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }))));
+                    try
+                    {
+                        using (var tumblrClient = new TumblrClientFactory().Create<TumblrClient>(
+                            ConfigurationManager.AppSettings["TumblrClientId"],
+                            ConfigurationManager.AppSettings["TumblrSecret"],
+                            new Token(
+                            ConfigurationManager.AppSettings["TumblrOAuthToken"],
+                            ConfigurationManager.AppSettings["TumblrOAuthSecret"])))
+                        {
+                            var tags = new List<string> { "frtools", "fr tools", "flight rising", "flightrising", "fr", "flash sale", "flashsale", item.Name.ToLower() };
+
+                            string body = $"<p>A new flash sale has been discovered for <b>{item.Name}</b></p>";
+                            body += $"<p><i>{item.Description}</i></p><br/>";
+                            body += "<p>";
+                            body += $"Game database: <a href=\"{string.Format(FRHelpers.GameDatabaseUrl, item.FRId)}\">click here</a><br/>";
+                            body += $"Marketplace link: <a href=\"{link}\">click here</a><br/>";
+                            body += "</p>";
+
+                            if (item.TreasureValue > 0)
+                                body += $"Treasure: <strike>{item.TreasureValue * 10}</strike> <b>{item.TreasureValue * .8 * 10}</b><br/>";
+
+                            if (item.FoodValue > 0)
+                                body += $"Food: {item.FoodValue}<br/>";
+
+                            string itemUrl = null;
+                            var random = new Random();
+                            switch (item.ItemCategory)
+                            {
+                                case FRItemCategory.Skins:
+                                    var skinType = item.ItemType.Split(' ');
+                                    var dragonType = (DragonType)Enum.Parse(typeof(DragonType), skinType[0]);
+                                    var gender = (Gender)Enum.Parse(typeof(Gender), skinType[1]);
+                                    itemUrl = string.Format(SiteHelpers.DummyDragonSkinProxyUrl, (int)dragonType, (int)gender, $"{item.FRId}");
+                                    body += $"For: {dragonType} {gender}<br/>";
+
+                                    var username = Regex.Match(item.Description, @"Designed by ([^.]+)[.|\)]");
+                                    if (username.Success)
+                                    {
+                                        var frUser = await FRHelpers.GetOrUpdateFRUser(username.Groups[1].Value);
+                                        if (frUser != null)
+                                            body += $"Created by: {frUser.Username}";
+                                    }
+
+                                    tags.Add("fr skins and accents");
+                                    tags.Add("fr skins");
+                                    tags.Add("fr accents");
+                                    tags.Add("fr skin");
+                                    tags.Add("fr accent");
+                                    tags.Add("skins and accents");
+                                    break;
+                                case FRItemCategory.Equipment:
+                                    {
+                                        var modernBreeds = GeneratedFRHelpers.GetModernBreeds();
+                                        itemUrl = string.Format(SiteHelpers.DummyDragonApparelProxyUrl, (int)modernBreeds[random.Next(1, modernBreeds.Length)], random.Next(0, 2), $"{item.FRId}");
+                                        tags.Add(item.ItemType.ToLower());
+                                        break;
+                                    }
+                                case FRItemCategory.Familiar:
+                                    itemUrl = string.Format(FRHelpers.FamiliarArtUrl, item.FRId);
+                                    tags.Add("fr familiar");
+                                    tags.Add("familiar");
+                                    break;
+                                case FRItemCategory.Trinket when item.ItemType == "Specialty Item" && (item.Name.StartsWith("Primary") || item.Name.StartsWith("Secondary") || item.Name.StartsWith("Tertiary")):
+                                    tags.Add($"{(item.Name.StartsWith("Primary") ? "primary" : item.Name.StartsWith("Secondary") ? "secondary" : "tertiarty")} gene");
+                                    tags.Add("gene");
+                                    var ancientBreed = GeneratedFRHelpers.GetAncientBreeds().Where(x => item.Name.EndsWith($"({x})"));
+                                    if (ancientBreed.Any())
+                                    {
+                                        itemUrl = string.Format(SiteHelpers.DummyDragonGeneProxyUrl, (int)ancientBreed.First(), random.Next(0, 2), $"{item.FRId}");
+                                        tags.Add("ancient gene");
+                                    }
+                                    else
+                                    {
+                                        var modernBreeds = GeneratedFRHelpers.GetModernBreeds();
+                                        itemUrl = string.Format(SiteHelpers.DummyDragonGeneProxyUrl, (int)modernBreeds[random.Next(1, modernBreeds.Length)], random.Next(0, 2), $"{item.FRId}");
+                                    }
+                                    break;
+                                default:
+                                    if (item.ItemType == "Scene")
+                                    {
+                                        itemUrl = string.Format(FRHelpers.SceneArtUrl, item.FRId);
+                                        tags.Add("scene");
+                                    }
+                                    else
+                                    {
+                                        if (item.AssetUrl != null)
+                                        {
+                                            Console.WriteLine("Unknown art type, attempting AssetURL");
+                                            itemUrl = $"https://www1.flightrising.com{item.AssetUrl}";
+                                            tags.Add(item.ItemType.ToLower());
+                                        }
+                                    }
+                                    break;
+                            }
+
+                            body += $"<img src=\"{itemUrl ?? string.Format(SiteHelpers.IconProxyUrl, item.Id)}\"/>";
+
+                            var post = PostData.CreateText(body, $"New Flash Sale: {item.Name}", tags);
+                            var postParamaters = (MethodParameterSet)typeof(PostData).GetField("parameters", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(post);
+                            postParamaters.Add("native_inline_images", true);
+                            await tumblrClient.CreatePostAsync("frtools", post);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+
                     item.FlashSales.Add(new FRItemFlashSale
                     {
                         DiscoveredAt = DateTime.UtcNow
