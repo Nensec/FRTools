67dd6008152794a37026fe81f50ceffccc81bf57
Perry van Velzen <perryvvelzen@gmail.com>
2024-01-27 00:04:35 +0100

    Trying this to keep the site from going to sleep as it makes Discord commands fail due to the stringent timeout that Discord enforces

---
+++ FRTools.Core.Functions/Workers/GeneralFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace FRTools.Core.Functions.Workers
+{
+    public class GeneralFunctions
+    {
+        [FunctionName(nameof(KeepSiteHot))]
+        public Task KeepSiteHot([TimerTrigger("0 */4 * * * *")] TimerInfo timer, ILogger log) => Task.CompletedTask;
+    }
+}
